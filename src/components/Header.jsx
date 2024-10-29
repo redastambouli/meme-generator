9a05e1315f53965007d5acb67ea0fc7f76ed0272
redastambouli <reda.boudghenestambouli@gmail.com>
2024-10-29 16:30:14 +0100

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import face from '/src/assets/Cry-Laugh-Emoji.png';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src={face}
+                className='header--image'
+            />
+            <h2 className='header--title'>Meme Generator</h2>
+            <h4 className='Header--project'>React Course - Project 3</h4>
+        </header>
+    )
+}
+
+export default Header;
