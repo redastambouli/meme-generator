f090e8fb0128eb23687d3d09fe37e6fe17868848
redastambouli <reda.boudghenestambouli@gmail.com>
2024-11-03 11:20:31 +0100

    Second commit

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,28 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../../memesData";
 
 const Meme = () => {
+
+    function getMemeImage() {
+        const memes = memesData.data.memes
+        const random = Math.floor(Math.random() * memes.length)
+        const url = memes[random].url
+        setMemeImage(url)
+    }
+
+    const [memeImage, setMemeImage] = useState('')
+
     return (
         <main>
-            <form className="form">
-                <input 
-                   type="text"
-                   className="form--input"
-                   placeholder="Top text"
-                   
-                   />
-                <input 
-                   type="text"
-                   className="form--input"
-                   placeholder="Bottom text"  
-                   
-                   />
-                <button className="form--button">
+            <div className="form">
+                <div>
+                    <label htmlFor="top-text" className="form--label">Top text</label>
+                    <input
+                        type="text"
+                        className="form--input"
+                        placeholder="Shut up"
+                        id="top-text"
+
+                    />
+
+                </div>
+                <div>
+                    <label htmlFor="bottom-text" className="form--label">Bottom Text</label>
+                    <input
+                        type="text"
+                        className="form--input"
+                        placeholder="and take my money"
+                        id="bottom-text"
+                    />
+
+                </div>
+
+                <button
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
                     Get a new meme image :)
                 </button>
-
-            </form>
+            </div>
+            <img src={memeImage} className="meme--image" />
         </main>
-        
+
     )
 }
 
